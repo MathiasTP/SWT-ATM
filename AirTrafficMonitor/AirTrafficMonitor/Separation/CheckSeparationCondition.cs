e44bb0ed66253142c047a78f5dc023257ae0ab10
Elgaard1 <andreas.e.sorensen@hotmail.com>
2019-10-30 09:36:22 +0100

    the first ørken

---
+++ AirTrafficMonitor/AirTrafficMonitor/Separation/CheckSeparationCondition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirTrafficMonitor.Converter;
+
+namespace AirTrafficMonitor.Separation
+{
+    public class CheckSeparationCondition : ICheckSeparationCondition
+    {
+        private ICheckSeparationCondition _reciever;
+        private List<IAirplane> separated;
+
+        public CheckSeparationCondition()
+        {
+
+        }
+
+        public void OnPlanesToLog(SeparationEventArgs j)
+        {
+
+        }
+    }
+}
